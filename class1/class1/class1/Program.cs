941d5a15010058c9505bfe748b1acf864faf760d
Ferhat Arıkoğlu <34ferhatarikoglu34@gmail.com>
2022-09-29 19:16:55 +0300

    c# ile nesne türetme

---
+++ class1/class1/class1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sınıfdan nesne nasıl turetilir adlı calışma
+            araba a = new araba(); //arabadan nesne turettikı
+            a.renk = "mavi";
+            a.otomatikmi = "araç otomatik ileri";
+            a.km = 1500000;
+
+            Console.WriteLine("arabanın rengi : " + a.renk);
+            Console.WriteLine("arabanın r : " + a.otomatikmi);
+            Console.WriteLine("arabanın kmsi : " + a.km);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ class1/class1/class1/araba.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class1
+{
+    public  class araba
+    {
+
+        public string renk { get; set; }
+        public string  otomatikmi{ get; set; }
+        public int km { get; set; }
+
+    }
+}
